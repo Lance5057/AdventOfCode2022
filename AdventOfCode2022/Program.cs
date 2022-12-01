7e19ef0f238bd623fbf8a844bc5c0991c2f68dd7
Lance5057 <Lance5057@gmail.com>
2022-12-01 10:25:12 -0600

    Add project files.

---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            day1.run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AdventOfCode2022/days/day1/day1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class day1
+    {
+        public static void run()
+        {
+            string loc = "C:/Users/dnann/source/repos/AdventOfCode2022/AdventOfCode2022/days/day1/input.txt";
+            List<int> elves = new List<int>();
+
+            if (File.Exists(loc))
+            {
+                using (StreamReader sr = File.OpenText(loc))
+                {
+                    string s;
+                    int i = 0;
+                    elves.Add(0);
+
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        if (s == "")
+                        {
+                            i++;
+                            elves.Add(0);
+                        }
+                        else
+                        {
+                            elves[i] += int.Parse(s);
+                        }
+
+                        //Console.WriteLine(s);
+                    }
+
+                    elves = elves.OrderByDescending(e => e).ToList();
+
+                    Console.WriteLine("Top: " + elves[0]);
+
+                    int combine = elves[0] + elves[1] + elves[2];
+                    Console.WriteLine("Top 3: " + combine);
+                }
+            }
+        }
+    }
+}
